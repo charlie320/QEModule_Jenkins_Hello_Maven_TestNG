6e1d50c70c156a842e89fae6263b7002525c0dd8
Charlie de la Rosa <jcdelarosa@verizon.net>
2020-03-05 09:27:18 -0600

    Initial commit

---
+++ QEModule_Jenkins_Hello_Maven_TestNG/src/test/java/com/perscholas/jenkins_hello/testng/HelloJenkinsTests.java
@@ -0,0 +1,26 @@
+package com.perscholas.jenkins_hello.testng;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import com.perscholas.jenkins_hello.HelloJenkins;
+
+public class HelloJenkinsTests {
+	private String actual = null;
+	private String expected = null;
+	
+	@Test
+	public void testHelloJenkins() {
+		actual = HelloJenkins.helloJenkins();
+		expected = "Hello Jenkins!";
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void testHelloJenkinsAgain() {
+		actual = HelloJenkins.helloJenkinsAgain();
+		expected = "Hello Jenkins again!!!";
+		assertEquals(actual, expected);
+	}
+}
